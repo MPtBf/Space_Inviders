790aa07f3c637babae64ae0ab8dca9263a221c84
ediko <morpho44p@gmail.com>
2023-03-23 23:55:30 +0500

    Добавлены игрок, враги и их движение

---
+++ setts.py
@@ -0,0 +1,35 @@
+# настройки игры
+WIDTH = 800
+HALF_WIDTH = WIDTH//2
+HEIGHT = 600
+HALF_HEIGHT = HEIGHT//2
+FPS = 60
+
+
+
+bg_img_mult = 1
+
+
+player_speed = 5
+enemy_speed = 3
+enemy_speed_down = 70
+
+
+fps_location = (WIDTH-95, 5)
+play_pause_location = (WIDTH-150,0)
+
+# цвета
+WHITE = (255,255,255)
+LGRAY = (193,193,193)
+GRAY = (127,127,127)
+DGRAY = (63,63,63)
+BLACK = (0,0,0)
+
+RED = (255,0,0)
+ORANGE = (255,127,0)
+YELLOW = (255,255,0)
+GREEN = (0,255,0)
+LBLUE = (0,255,255)
+BLUE = (0,0,255)
+PINK = (255,0,255)
+PURPLE = (127,0,127)
